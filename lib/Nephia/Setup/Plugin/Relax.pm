4d5a5029844ea437786a94baea06b37b6056b578
ytnobody <ytnobody@gmail.com>
2013-11-30 15:04:30 +0900

    typo

--- lib/Nephia/Setup/Plugin/Relax.pm
+++ lib/Nephia/Setup/Plugin/Relax.pm
@@ -127,7 +127,7 @@ use Plack::Builder;
 use Plack::Session::Store::Cache;
 use Cache::Memcached::Fast;
 
-my $run_env       = $ENV{PLACK_ENV} || 'local';
+my $run_env       = $ENV{PLACK_ENV} eq 'development' ? 'local' : $ENV{PLACK_ENV};
 my $basedir       = dirname(__FILE__);
 my $config_file   = File::Spec->catfile($basedir, 'config', $run_env.'.pl');
 my $config        = require($config_file);
